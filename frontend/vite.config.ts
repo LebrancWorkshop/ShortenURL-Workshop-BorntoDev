9aa3c798283203c80e568401126e32e15ee55407
lebrancconvas <lebrancconvas@gmail.com>
2023-06-01 02:29:27 +0700

    🔧 chore(config): config base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api/v1/url': {
+        target: 'http://localhost:8101',
+        changeOrigin: true,
+      }
+    }
+  }
+});
