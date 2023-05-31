338ace98e2a52c63df161a73ee6c142e38d52417
lebrancconvas <lebrancconvas@gmail.com>
2023-05-31 21:45:59 +0700

    ✨ feat: update function on front-end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,12 +11,24 @@ const App: React.FC = () => {
     try {
       const response = await axios.post('/api/v1/url', { url });
       const { short_url } = response.data;
-      setShortURL(short_url); 
+      setShortURL(short_url);
     } catch (error) {
       console.error(error);
     }
 
   }
+
+  const handleOpenUrl = () => {
+    if(shortURL) {
+      window.open(`http://localhost:8101/api/v1/${shortURL}`, '_blank');
+    }
+  };
+
+  const handleCopyUrl = () => {
+    if(shortURL) {
+      navigator.clipboard.writeText(`http://localhost:8101/api/v1/${shortURL}`);
+    }
+  };
 
   return(
     <div>
@@ -27,10 +39,19 @@ const App: React.FC = () => {
       </header>
       <section id="form-section">
         <div>
-          <form action="POST">
-            <input type="text" name="url" id="url" placeholder="Enter URL here" />
+          <form onSubmit={handleSubmit}>
+            <input type="text" value={url} onChange={(event) => setURL(event.target.value)} id="url" placeholder="Enter URL here" />
             <input type="submit" value="submit" />
           </form>
+          {shortURL && (
+            <div>
+              <div>
+                <p>Shorten URL: <a href={`http://localhost:8101/api/v1/${shortURL}`}></a></p>
+                <button onClick={handleOpenUrl}>Open</button>
+                <button onClick={handleCopyUrl}>Copy</button>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </div>
